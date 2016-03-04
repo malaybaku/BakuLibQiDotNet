f0156a2799ba8245ded111bceadcf896b2a3772b
malaybaku <njslyr.trpg+harriet@gmail.com>
2016-03-04 13:12:47 +0900

    add build configuration x64/AnyCPU (for other platforms like Unity)

--- Baku.LibqiDotNet/Baku.LibqiDotNet/QiApi/DllImportSettings.cs
+++ Baku.LibqiDotNet/Baku.LibqiDotNet/QiApi/DllImportSettings.cs
@@ -6,6 +6,8 @@
         /// <summary>ライブラリが読み込む予定のアンマネージドDLLファイルの名前</summary>
 #if UNITY
         public const string DllName = @"qic";
+#elif LINUX
+        public const string DllName = @"libqic.so";
 #else
         public const string DllName = @"qic.dll";
 #endif
