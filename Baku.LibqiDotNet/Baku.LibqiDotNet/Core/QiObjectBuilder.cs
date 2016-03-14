393fc4aaf24c122dc842d9f639578a28e9adc683
malaybaku <njslyr.trpg+harriet@gmail.com>
2016-03-15 01:24:29 +0900

    add method advertising logic (using generetable signature)

--- Baku.LibqiDotNet/Baku.LibqiDotNet/Core/QiObjectBuilder.cs
+++ Baku.LibqiDotNet/Baku.LibqiDotNet/Core/QiObjectBuilder.cs
@@ -34,6 +34,27 @@ namespace Baku.LibqiDotNet
             _advertisedMethods[id] = qiMethod;
         }
 
+        /// <summary>関数を登録します。</summary>
+        /// <param name="methodName">関数の名前</param>
+        /// <param name="returns">返却する値の型</param>
+        /// <param name="args">引数の型一覧</param>
+        /// <param name="method">メソッドの実装</param>
+        public void AdvertiseMethod(
+            string methodName,
+            QiMethodSignature returns,
+            IEnumerable<QiMethodSignature> args,
+            QiObjectMethod method
+            )
+        {
+            string signature = methodName +
+                QiSignatures.MethodNameSuffix +
+                returns.Signature +
+                QiMethodSignature.TupleOf(args);
+
+            AdvertiseMethod(signature, method);
+        }
+
+
         /// <summary>シグナルを登録します。</summary>
         /// <param name="name">シグナル名</param>
         /// <param name="signature">シグネチャ</param>
