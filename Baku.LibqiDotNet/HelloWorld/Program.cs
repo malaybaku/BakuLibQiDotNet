61ade8719b6656afdfe7831e11cbdd5d1ae1bce2
malaybaku <njslyr.trpg+harriet@gmail.com>
2016-07-18 16:46:27 +0900

    fixed Hello World (previous version had temporal debug code)

--- Baku.LibqiDotNet/HelloWorld/Program.cs
+++ Baku.LibqiDotNet/HelloWorld/Program.cs
@@ -8,14 +8,11 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            //作業ディレクトリだけでなく"dlls"というフォルダのライブラリも実行中に参照できるよう設定を変更
+            //ネイティブライブラリを利用する場合に備えてネイティブライブラリの置き場所候補を増やす
             PathModifier.AddEnvironmentPath("dlls", PathModifyMode.RelativeToEntryAssembly);
 
-            //HelloWorldの対象とするマシンのIPアドレスを指定(ポートは既定値のHTTP:8002/TCP:9559を使用)
-            var session = QiSession.CreateSocketIoSession();
-            session.Protocol = "http://";
-            session.Port = QiSession.DefaultHttpPort;
-            session.Connect("192.168.1.3");
+            var session = QiSession.CreateSession();
+            session.Connect("127.0.0.1");
 
             Console.WriteLine($"Connected? {session.IsConnected}");
             if (!session.IsConnected)
@@ -24,14 +21,12 @@ namespace HelloWorld
                 return;
             }
 
+            Console.WriteLine("Robot will say 'this is test.'");
+
             //最も基本的なモジュールの一つとして合成音声のモジュールを取得
             var tts = session.GetService("ALTextToSpeech");
-
             //"say"関数に文字列引数を指定して実行
             tts["say"].Call("this is test");
-
-            var tts2 = session.GetService("ALTextToSpeech");
-            tts2["say"].Call("this is also test, do you understand?");
 
             session.Close();
         }
